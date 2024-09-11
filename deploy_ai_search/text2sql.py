c9ac6233bc9269788e9ef5652e0781521278f900
Ben Constable <benconstable@microsoft.com>
2024-09-11 01:31:05 +0100

    Work in progress

--- deploy_ai_search/text2sql.py
+++ deploy_ai_search/text2sql.py
@@ -9,18 +9,21 @@ from azure.search.documents.indexes.models import (
     SemanticPrioritizedFields,
     SemanticConfiguration,
     SemanticSearch,
+    SimpleField
 )
 from ai_search import AISearch
 from environment import (
     IndexerType,
 )
+import logging
+import json
 
 
-class RagDocumentsAISearch(AISearch):
+class Text2SqlAISearch(AISearch):
     """This class is used to deploy the sql index."""
 
     def __init__(self, suffix: str | None = None, rebuild: bool | None = False):
-        """Initialize the RagDocumentsAISearch class. This class implements the deployment of the sql index.
+        """Initialize the Text2SqlAISearch class. This class implements the deployment of the sql index.
 
         Args:
             suffix (str, optional): The suffix for the indexer. Defaults to None. If an suffix is provided, it is assumed to be a test indexer.
@@ -28,6 +31,8 @@ class RagDocumentsAISearch(AISearch):
         """
         self.indexer_type = IndexerType.SQL
         super().__init__(suffix, rebuild)
+
+        self.entities = []
 
     def get_index_fields(self) -> list[SearchableField]:
         """This function returns the index fields for sql index.
@@ -44,6 +49,9 @@ class RagDocumentsAISearch(AISearch):
             ),
             SearchableField(
                 name="EntityName", type=SearchFieldDataType.String, filterable=True
+            ),
+            SimpleField(
+                name="SelectFromEntity", type=SearchFieldDataType.String
             ),
             SearchableField(
                 name="Description",
@@ -103,3 +111,47 @@ class RagDocumentsAISearch(AISearch):
         semantic_search = SemanticSearch(configurations=[semantic_config])
 
         return semantic_search
+
+    def load_entities(self):
+        """Load the views from the JSON file and formats into memory."""
+        with open("../text2sql/plugins/vector_based_sql_plugin/entities.json", "r", encoding="utf-8") as file:
+            entities = json.load(file)
+
+            def rename_keys(d: dict, key_mapping: dict) -> dict: 
+                """Rename the keys in the dictionary.
+                
+                Args:
+                    d (dict): The dictionary to rename the keys.
+                    key_mapping (dict): The mapping of the keys to rename.
+                    
+                Returns:
+                    dict: The dictionary with the renamed keys.
+                """
+                return {key_mapping.get(k): v for k, v in d.items()}
+
+            top_level_renaming_map = {"view_name": "EntityName", "table_name": "EntityName", "entity": "Entity", "columns": "Columns", "description": "Description", "selector": "Selector"}
+
+            # Load tables and views
+            for entity in entities["tables"].extend(entities["views"]):
+                entity_object = rename_keys(entity.copy(), top_level_renaming_map)
+
+                entity = entity_object["Entity"]
+                entity_object["SelectFromEntity"] = f"{self.database}.{entity}"
+
+                entity_object["Columns"] = rename_keys(entity_object["Columns"], {"name": "Name", "definition": "Definition", "type": "Type"})
+                self.entities.append(entity_object)
+
+        logging.info("Entities loaded into memory.")
+
+    def deploy_entities(self):
+        """Upload the entities to AI search"""
+
+        self.index_client.upload_documents(documents=self.entities)
+
+        logging.info("Entities uploaded to AI search.")
+
+    def deploy(self):
+        """Deploy the sql index."""
+        super().deploy()
+        self.load_entities()
+        self.deploy_entities()
