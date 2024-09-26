b0aeac11c02612faecaef8ecb124d17bb71a9600
Ben Constable <benconstable@microsoft.com>
2024-09-26 18:05:30 +0100

    Further work on metadata improvements

--- text_2_sql/data_dictionary/data_dictionary_creator.py
+++ text_2_sql/data_dictionary/data_dictionary_creator.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import aioodbc
 import os
+import asyncio
 
 
 class DataDictionaryCreator(ABC):
@@ -12,7 +13,6 @@ class DataDictionaryCreator(ABC):
         """An abstract property to extract table entities from a database.
 
         Must return 3 columns: Entity, Schema, Description."""
-        pass
 
     @property
     @abstractmethod
@@ -20,9 +20,19 @@ class DataDictionaryCreator(ABC):
         """An abstract property to extract view entities from a database.
 
         Must return 3 columns: Entity, Schema, Description."""
-        pass
+
+    @abstractmethod
+    def extract_columns_sql_query(self, entity, schema) -> str:
+        """An abstract method to extract column information from a database.
+
+        Must return 3 columns: Name, Type, Definition."""
+
+    def extract_distinct_values_sql_query(self, entity, schema, column) -> str:
+        """A method to extract distinct values from a column in a database."""
+        return f"""SELECT DISTINCT {column} FROM {schema}.{entity};"""
 
     async def query_entities(self, sql_query: str):
+        """A method to query a database for entities. Can be sub-classed if needed."""
         connection_string = os.environ["Text2Sql__DatabaseConnectionString"]
         async with await aioodbc.connect(dsn=connection_string) as sql_db_client:
             async with sql_db_client.cursor() as cursor:
@@ -44,3 +54,29 @@ class DataDictionaryCreator(ABC):
         all_entities = table_entities + view_entities
 
         return all_entities
+
+    async def extract_column_distinct_values(self, entity, schema, column):
+        distinct_values = await self.query_entities(
+            self.extract_distinct_values_sql_query(entity, schema, column)
+        )
+
+        return distinct_values
+
+    async def extract_columns_with_definitions(self, entity, schema):
+        """A method to extract column information from a database."""
+        columns = await self.query_entities(
+            self.extract_columns_sql_query(entity, schema)
+        )
+
+        distinct_value_tasks = []
+        for column in columns:
+            distinct_value_tasks.append(
+                self.extract_column_distinct_values(entity, schema, column["Name"])
+            )
+
+        distinct_values = await asyncio.gather(*distinct_value_tasks)
+
+        for column, distinct_values_for_column in zip(columns, distinct_values):
+            column["DistinctValues"] = distinct_values_for_column
+
+        return columns
--- text_2_sql/data_dictionary/sql_sever_data_dictionary_creator.py
+++ text_2_sql/data_dictionary/sql_sever_data_dictionary_creator.py
@@ -37,3 +37,21 @@ LEFT JOIN
     AND ep.minor_id = 0
     AND ep.class = 1
     AND ep.name = 'MS_Description';"""
+
+    def extract_columns_sql_query(self, entity, schema) -> str:
+        """A property to extract column information from a SQL Server database."""
+        return f"""SELECT
+    c.COLUMN_NAME AS Name,
+    c.DATA_TYPE AS Type,
+    CAST(ep.value AS NVARCHAR(500)) AS Definition
+FROM
+    INFORMATION_SCHEMA.COLUMNS c
+LEFT JOIN
+    sys.extended_properties ep
+    ON ep.major_id = OBJECT_ID(c.TABLE_SCHEMA + '.' + c.TABLE_NAME)
+    AND ep.minor_id = c.ORDINAL_POSITION
+    AND ep.class = 1
+    AND ep.name = 'MS_Description'
+WHERE
+    c.TABLE_SCHEMA = '{schema}'
+    AND c.TABLE_NAME = '{entity}';"""
