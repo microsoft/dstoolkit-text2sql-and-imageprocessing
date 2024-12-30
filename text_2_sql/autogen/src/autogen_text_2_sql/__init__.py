e846e9e00aa1ae881c129f7f465f59615573951a
Ben Constable <benconstable@microsoft.com>
2024-12-30 15:59:02 +0000

    Update

---
+++ text_2_sql/autogen/src/autogen_text_2_sql/__init__.py
@@ -0,0 +1,3 @@
+from autogen_text_2_sql.autogen_text_2_sql import AutoGenText2Sql
+
+__all__ = ["AutoGenText2Sql"]
---
+++ text_2_sql/text_2_sql_core/src/text_2_sql_core/payloads/__init__.py
@@ -0,0 +1,4 @@
+from text_2_sql_core.payloads.answer_with_sources import AnswerWithSources, Source
+from text_2_sql_core.payloads.user_information_request import UserInformationRequest
+
+__all__ = ["AnswerWithSources", "Source", "UserInformationRequest"]
