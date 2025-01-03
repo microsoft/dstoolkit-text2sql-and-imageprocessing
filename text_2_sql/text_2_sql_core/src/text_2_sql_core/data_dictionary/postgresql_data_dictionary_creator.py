482a08d58117378243e45e1421ef707bdf491d86
Ben Constable <benconstable@microsoft.com>
2025-01-03 16:36:44 +0000

    Update postgresql commands

--- text_2_sql/text_2_sql_core/src/text_2_sql_core/data_dictionary/postgresql_data_dictionary_creator.py
+++ text_2_sql/text_2_sql_core/src/text_2_sql_core/data_dictionary/postgresql_data_dictionary_creator.py
@@ -24,64 +24,87 @@ class PostgresqlDataDictionaryCreator(DataDictionaryCreator):
     def extract_table_entities_sql_query(self) -> str:
         """A property to extract table entities from a PostgreSQL database."""
         return """SELECT
-            t.table_name AS entity,
-            t.table_schema AS entity_schema,
-            pg_catalog.col_description(c.oid, 0) AS definition
+            t.table_name AS "Entity",
+            t.table_schema AS "EntitySchema",
+            pg_catalog.obj_description(c.oid, 'pg_class') AS "Definition"
         FROM
             information_schema.tables t
         JOIN
             pg_catalog.pg_class c ON c.relname = t.table_name
             AND c.relnamespace = (SELECT oid FROM pg_catalog.pg_namespace WHERE nspname = t.table_schema)
+        LEFT JOIN
+            pg_catalog.pg_description pd ON pd.objoid = c.oid
         WHERE
             t.table_type = 'BASE TABLE'
-        ORDER BY entity_schema, entity;"""
+            AND pd.objsubid = 0  -- 0 indicates the table description, not column descriptions
+        ORDER BY
+            "EntitySchema", "Entity";"""
 
     @property
     def extract_view_entities_sql_query(self) -> str:
         """A property to extract view entities from a PostgreSQL database."""
         return """SELECT
-            v.view_name AS entity,
-            v.table_schema AS entity_schema,
-            pg_catalog.col_description(c.oid, 0) AS definition
+            v.viewname AS "Entity",
+            v.schemaname AS "EntitySchema",
+            pg_catalog.obj_description(c.oid, 'pg_class') AS "Definition"
         FROM
-            information_schema.views v
+            pg_catalog.pg_views v
         JOIN
-            pg_catalog.pg_class c ON c.relname = v.view_name
-            AND c.relnamespace = (SELECT oid FROM pg_catalog.pg_namespace WHERE nspname = v.table_schema)
-        ORDER BY entity_schema, entity;"""
+            pg_catalog.pg_class c ON c.relname = v.viewname
+            AND c.relnamespace = (SELECT oid FROM pg_catalog.pg_namespace WHERE nspname = v.schemaname)
+        LEFT JOIN
+            pg_catalog.pg_description pd ON pd.objoid = c.oid
+        WHERE
+            pd.objsubid = 0  -- 0 indicates the view description, not a column description
+        ORDER BY
+            "EntitySchema", "Entity";"""
 
     def extract_columns_sql_query(self, entity: EntityItem) -> str:
         """A property to extract column information from a PostgreSQL database."""
         return f"""SELECT
-            c.column_name AS name,
-            c.data_type AS data_type,
-            pg_catalog.col_description(t.oid, c.ordinal_position) AS definition
+            c.attname AS "Name",
+            t.typname AS "DataType",
+            pgd.description AS "Definition"
         FROM
-            information_schema.columns c
-        JOIN
-            pg_catalog.pg_class t ON t.relname = c.table_name
-            AND t.relnamespace = (SELECT oid FROM pg_catalog.pg_namespace WHERE nspname = c.table_schema)
+            pg_attribute c
+        INNER JOIN
+            pg_class tbl ON c.attrelid = tbl.oid
+        INNER JOIN
+            pg_namespace ns ON tbl.relnamespace = ns.oid
+        INNER JOIN
+            pg_type t ON c.atttypid = t.oid
+        LEFT JOIN
+            pg_description pgd ON pgd.objoid = tbl.oid AND pgd.objsubid = c.attnum
         WHERE
-            c.table_schema = '{entity.entity_schema}'
-            AND c.table_name = '{entity.name}'
-        ORDER BY c.ordinal_position;"""
+            ns.nspname = '{entity.entity_schema}'
+            AND tbl.relname = '{entity.name}'
+            AND c.attnum > 0  -- Exclude system columns
+        ORDER BY
+            c.attnum;"""
 
     @property
     def extract_entity_relationships_sql_query(self) -> str:
         """A property to extract entity relationships from a PostgreSQL database."""
         return """SELECT
-            tc.table_schema AS entity_schema,
-            tc.table_name AS entity,
-            rc.unique_constraint_schema AS foreign_entity_schema,
-            rc.unique_constraint_name AS foreign_entity_constraint,
-            rc.constraint_name AS foreign_key_constraint
+            fk_schema.nspname AS "EntitySchema",
+            fk_tab.relname AS "Entity",
+            pk_schema.nspname AS "ForeignEntitySchema",
+            pk_tab.relname AS "ForeignEntity",
+            fk_col.attname AS "Column",
+            pk_col.attname AS "ForeignColumn"
         FROM
-            information_schema.referential_constraints rc
-        JOIN
-            information_schema.table_constraints tc
-            ON rc.constraint_schema = tc.constraint_schema
-            AND rc.constraint_name = tc.constraint_name
-        WHERE
-            tc.constraint_type = 'FOREIGN KEY'
+            pg_constraint fk
+        INNER JOIN
+            pg_attribute fk_col ON fk.conrelid = fk_col.attrelid AND fk.attnum = fk_col.attnum
+        INNER JOIN
+            pg_class fk_tab ON fk.conrelid = fk_tab.oid
+        INNER JOIN
+            pg_namespace fk_schema ON fk_tab.relnamespace = fk_schema.oid
+        INNER JOIN
+            pg_class pk_tab ON fk.confrelid = pk_tab.oid
+        INNER JOIN
+            pg_namespace pk_schema ON pk_tab.relnamespace = pk_schema.oid
+        INNER JOIN
+            pg_attribute pk_col ON fk.confrelid = pk_col.attrelid AND fk.confkey[1] = pk_col.attnum
         ORDER BY
-            entity_schema, entity, foreign_entity_schema, foreign_entity_constraint;"""
+            "EntitySchema", "Entity", "ForeignEntitySchema", "ForeignEntity";"""
