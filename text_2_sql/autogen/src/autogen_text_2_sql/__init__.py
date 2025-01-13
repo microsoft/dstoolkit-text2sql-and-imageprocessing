00e902455f2c53a1085d810faf3da327932b4c07
Ben Constable <benconstable@microsoft.com>
2025-01-13 12:36:09 +0000

    Update payloads

--- text_2_sql/autogen/src/autogen_text_2_sql/__init__.py
+++ text_2_sql/autogen/src/autogen_text_2_sql/__init__.py
@@ -1,6 +1,19 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 from autogen_text_2_sql.autogen_text_2_sql import AutoGenText2Sql
-from text_2_sql_core.payloads.interaction_payloads import UserMessagePayload
+from text_2_sql_core.payloads.interaction_payloads import (
+    UserMessagePayload,
+    DismabiguationRequestsPayload,
+    AnswerWithSourcesPayload,
+    ProcessingUpdatePayload,
+    InteractionPayload,
+)
 
-__all__ = ["AutoGenText2Sql", "UserMessagePayload"]
+__all__ = [
+    "AutoGenText2Sql",
+    "UserMessagePayload",
+    "DismabiguationRequestsPayload",
+    "AnswerWithSourcesPayload",
+    "ProcessingUpdatePayload",
+    "InteractionPayload",
+]
