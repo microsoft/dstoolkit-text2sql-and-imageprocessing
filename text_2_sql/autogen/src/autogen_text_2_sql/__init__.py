aa36e62385c4d432288f398a10a5d2c1815e8867
krisnyl <58690365+krisnyl@users.noreply.github.com>
2025-01-29 17:01:17 +0200

    Add state store for Autogen (#150)
    * added state store
    
    * added expiry for in-memory state store
    
    * remove test.py
    
    * type annotations
    
    * -
    
    * Update notebook sample
    
    * Update README
    
    * Run formatter
    
    ---------
    
    Co-authored-by: Kristian Nylund <Kristian.Nylund@microsoft.com>
    Co-authored-by: Ben Constable <benconstable@microsoft.com>

--- text_2_sql/autogen/src/autogen_text_2_sql/__init__.py
+++ text_2_sql/autogen/src/autogen_text_2_sql/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 from autogen_text_2_sql.autogen_text_2_sql import AutoGenText2Sql
+from autogen_text_2_sql.state_store import InMemoryStateStore
+
 from text_2_sql_core.payloads.interaction_payloads import (
     UserMessagePayload,
     DismabiguationRequestsPayload,
@@ -16,4 +18,5 @@ __all__ = [
     "AnswerWithSourcesPayload",
     "ProcessingUpdatePayload",
     "InteractionPayload",
+    "InMemoryStateStore",
 ]
